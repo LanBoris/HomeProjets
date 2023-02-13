27e9e7600f8aea26364a1ef8c457482f948add31
Boris Lanovenko <blanovenko@gmail.com>
2023-02-13 21:03:40 +0300

    Create homeProject01 change 1

--- homeProject01/Program.cs
+++ homeProject01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Create a matrix symbols.
+
+Console.Clear();
+
+int ind = 0;
+Console.Write("Enter a count = ");
+int count = Convert.ToInt32(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.Green;
+
+while (ind < count)
+{
+  int symbol = new Random().Next(1, 100);
+  Console.Write(symbol + " ");
+  ind++;
+}
+Console.ForegroundColor=ConsoleColor.White;
