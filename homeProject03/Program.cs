1ea8a6fe69cb53e22915f54e60e43b04daab07bc
Boris Lanovenko <blanovenko@gmail.com>
2023-02-14 18:13:43 +0300

    Create homeProject03 change 2

--- homeProject03/Program.cs
+++ homeProject03/Program.cs
@@ -1,3 +1,6 @@
-﻿Console.Write("Введите целое число = ");
-int number=Convert.ToInt32(Console.ReadLine());
-Console.Write("Последняя цифра = " + number % 10);
+﻿// Console.Write("Введите целое число = ");
+// int number=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Последняя цифра = " + number % 10);
+
+// Console.WriteLine("Введите целое число");
+// Console.WriteLine("Число десятков = " + int.Parse(Console.ReadLine()) / 10 % 10);
