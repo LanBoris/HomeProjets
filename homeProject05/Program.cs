751790e55515c5fa67d9fe68566d214e8ea574e6
Boris Lanovenko <blanovenko@gmail.com>
2023-02-18 01:33:02 +0300

    Create homeProject05

---
+++ homeProject05/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("N: ");
+int N=Convert.ToInt32(Console.ReadLine());
+
+while (N<=0)
+{
+  Console.Write("New N: ");
+  N=Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(Math.Pow(N, 3));
