869b8134cab329e4b92ae2886cd631ee623db3d6
Boris Lanovenko <blanovenko@gmail.com>
2023-02-19 01:14:50 +0300

    Create homeProject06 change 1

--- homeProject06/Program.cs
+++ homeProject06/Program.cs
@@ -5,3 +5,46 @@
 
 // [591, 532, 189, 523, 333, 546, 527, 275, 456, 264]
 
+// string stringNumber = number.ToString();
+// char[] sN = stringNumber.ToCharArray();
+// if (sN.Length % 2 == 1)
+// {
+//   char cD = sN[sN.Length / 2];
+//   int centerDigit = (int)Char.GetNumericValue(cD);
+
+
+for (int num = 10; num <= 999; num++)
+{
+  string sn = num.ToString();
+  char[] n = sn.ToCharArray();
+  if (n.Length == 2)
+  {
+    char n1 = sn[0];
+    char n2 = sn[1];
+    int in1 = (int)Char.GetNumericValue(n1);
+    int in2 = (int)Char.GetNumericValue(n2);
+    if ((in1 * in2) % (in1 + in2) == 0)
+    {
+      if ((in1 * in2) / (in1 + in2) > 0)
+      {
+        Console.Write(num + "-");
+      }
+    }
+  }
+  else if (n.Length == 3)
+  {
+    char n1 = sn[0];
+    char n2 = sn[1];
+    char n3 = sn[2];
+    int in1 = (int)Char.GetNumericValue(n1);
+    int in2 = (int)Char.GetNumericValue(n2);
+    int in3 = (int)Char.GetNumericValue(n3);
+    if ((in1 * in2 * in3) % (in1 + in2 + in3) == 0)
+    {
+      if ((in1 * in2 * in3) / (in1 + in2 + in3) > 0)
+      {
+        Console.Write(num + "-");
+      }
+    }
+  }
+}
