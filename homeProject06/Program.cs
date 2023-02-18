ccb5db6b8936565aa601868e026f1be343b2b94c
Boris Lanovenko <blanovenko@gmail.com>
2023-02-19 01:57:30 +0300

    Create homeProject06 change 2

--- homeProject06/Program.cs
+++ homeProject06/Program.cs
@@ -15,6 +15,7 @@
 
 for (int num = 10; num <= 999; num++)
 {
+  int numb=0;
   string sn = num.ToString();
   char[] n = sn.ToCharArray();
   if (n.Length == 2)
@@ -23,12 +24,9 @@ for (int num = 10; num <= 999; num++)
     char n2 = sn[1];
     int in1 = (int)Char.GetNumericValue(n1);
     int in2 = (int)Char.GetNumericValue(n2);
-    if ((in1 * in2) % (in1 + in2) == 0)
+    if ((in1 * in2) % (in1 + in2) == 0 && (in1 * in2) / (in1 + in2) > 0)
     {
-      if ((in1 * in2) / (in1 + in2) > 0)
-      {
-        Console.Write(num + "-");
-      }
+      numb=num;
     }
   }
   else if (n.Length == 3)
@@ -39,12 +37,20 @@ for (int num = 10; num <= 999; num++)
     int in1 = (int)Char.GetNumericValue(n1);
     int in2 = (int)Char.GetNumericValue(n2);
     int in3 = (int)Char.GetNumericValue(n3);
-    if ((in1 * in2 * in3) % (in1 + in2 + in3) == 0)
+    if ((in1 * in2 * in3) % (in1 + in2 + in3) == 0 && (in1 * in2 * in3) / (in1 + in2 + in3) > 0)
     {
-      if ((in1 * in2 * in3) / (in1 + in2 + in3) > 0)
-      {
-        Console.Write(num + "-");
-      }
+      numb=num;
     }
   }
+  if (numb>0)
+  {
+    Console.Write(numb + "-");
+  }
 }
+
+// int i =0;
+// int[] array=new int[10];
+// while (i<10)
+// {
+  
+// }
