f84a08f0c471e624db2b091f79034f92335511ec
Boris Lanovenko <blanovenko@gmail.com>
2023-02-19 01:59:57 +0300

    Create homeProject07

---
+++ homeProject07/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] arrayNumber = new int[10];
+for (int i = 0; i < 10; i++)
+{
+  while (true)
+  {
+    int number = new Random().Next(10, 1000); // 10-999
+    int summa = 0, multi = 0;
+    while (number > 0)
+    {
+      summa += summa % 10;
+      multi *= multi % 10;
+      number /= 10;
+    }
+
+    if ((multi % summa) == 0)
+    {
+      Console.WriteLine($"число {number}");
+      arrayNumber[i] = number;
+      break;
+    }
+  }
+}
