6ca2e032313cd828fb4dc441b5ef40183b5f4020
Boris Lanovenko <blanovenko@gmail.com>
2023-02-19 11:39:29 +0300

    Create homeProject08

---
+++ homeProject08/Program.cs
@@ -0,0 +1,8 @@
+﻿// Find a century
+
+Console.Write("Enter a year: ");
+int year=Convert.ToInt32(Console.ReadLine());
+
+int century = (year - 1) / 100 + 1;
+
+Console.WriteLine($"Century - {century}");
