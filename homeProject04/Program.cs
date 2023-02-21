59bf782b40c03e8d0870d184608ac98bb7c0a8d2
Boris Lanovenko <blanovenko@gmail.com>
2023-02-21 13:31:32 +0300

    Create homeProject04 change 2

--- homeProject04/Program.cs
+++ homeProject04/Program.cs
@@ -1,2 +1,12 @@
-﻿Console.Clear();
+﻿// Замена цифры в строке на 0 или 1
 
+Console.Clear();
+Console.Write("Введите строку цифр: ");
+string x = Console.ReadLine()!;
+string str = "";
+foreach (char s in x)
+{
+  if (s < '5') str += '0';
+  else str += '1';
+}
+Console.WriteLine(str);
