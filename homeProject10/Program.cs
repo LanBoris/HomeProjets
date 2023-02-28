3fced049ade5d89ce7a210038caf8b458eeacc10
Boris Lanovenko <blanovenko@gmail.com>
2023-03-01 00:55:17 +0300

    Create homeProject10

---
+++ homeProject10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = Convert.ToString(number, 2);
+Console.WriteLine($"В двоичной системе счисления это: {result}");
