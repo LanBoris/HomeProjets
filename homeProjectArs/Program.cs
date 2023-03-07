22ca8c16184f7f8965acfc0dfba1edbfb3a5e789
Boris Lanovenko <blanovenko@gmail.com>
2023-03-07 13:37:27 +0300

    Create ArsProject 2.0

--- homeProjectArs/Program.cs
+++ homeProjectArs/Program.cs
@@ -12,14 +12,48 @@ int number1 = 0;
 int number2 = 0;
 int operation = 0;
 string op = String.Empty;
+int[] answers = new int[count];
 for (int i = 0; i < count; i++)
 {
+  Console.ForegroundColor = ConsoleColor.Yellow;
+  int temp = 0;
   operation = new Random().Next(1, 3);
-  if (operation == 1) op = "+";
-  else if (operation == 2) op = "-";
-  number1 = new Random().Next(minValue, maxValue);
-  number2 = new Random().Next(minValue, maxValue);
-  if (number1>=number2) Console.WriteLine($"Пример №{i+1}: {number1}{op}{number2}");
-  else if (number1<=number2) Console.WriteLine($"Пример №{i+1}: {number2}{op}{number1}");
+  if (operation == 1)
+  {
+    op = "+";
+    number1 = new Random().Next(minValue, maxValue);
+    number2 = new Random().Next(minValue, maxValue);
+    if (number1 >= number2)
+    {
+      Console.WriteLine($"Пример №{i + 1}: {number1}{op}{number2}");
+      temp = number1 + number2;
+    }
+    else if (number1 <= number2)
+    {
+      Console.WriteLine($"Пример №{i + 1}: {number2}{op}{number1}");
+      temp = number2 + number1;
+    }
+  }
+  else if (operation == 2)
+  {
+    op = "-";
+    number1 = new Random().Next(minValue, maxValue);
+    number2 = new Random().Next(minValue, maxValue);
+    if (number1 >= number2)
+    {
+      Console.WriteLine($"Пример №{i + 1}: {number1}{op}{number2}");
+      temp = number1 - number2;
+    }
+    else if (number1 <= number2)
+    {
+      Console.WriteLine($"Пример №{i + 1}: {number2}{op}{number1}");
+      temp = number2 - number1;
+    }
+  }
+  answers[i] = temp;
 }
+Console.WriteLine("\n" + "Чтобы получить ответы на прмиеры, нажмите Enter.");
+Console.ReadKey();
+Console.WriteLine("\n" + $"Ответы: {String.Join("; ", answers)}");
+Console.ForegroundColor = ConsoleColor.White;
 
