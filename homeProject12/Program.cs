66c0e6ca8ed37d59c5ee07769274c1199678af4f
Boris Lanovenko <blanovenko@gmail.com>
2023-03-21 11:35:39 +0300

    Changes in homeProject12

--- homeProject12/Program.cs
+++ homeProject12/Program.cs
@@ -1,4 +1,14 @@
 ﻿
-var random = new Random();
-int[] array = Enumerable.Range(10,90).OrderBy(t => random.Next()).ToArray();
-Console.WriteLine(String.Join("; ", array));
+// var random = new Random();
+// int[] array = Enumerable.Range(10, 90).OrderBy(t => random.Next()).ToArray();
+// Console.WriteLine(String.Join("; ", array));
+
+// int[] array = { 1, 3, 4, 5, 4, 2 };
+// int item = 4;
+// array = Array.FindAll(array, i => i != item).ToArray();
+// Console.WriteLine(String.Join(",", array));
+
+int[] array = { 1, 3, 4, 5, 4, 33, 44, 12, 13, 4, 2 };
+int item = 4;
+array = array.Where(e => e != item).ToArray();
+Console.WriteLine(String.Join(",", array));
